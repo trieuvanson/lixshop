3293e3de883b5c030d4e81740d7b4eeeb9838778
trieuvanson <Vodanhserver@gmail.com>
2022-03-09 00:26:12 +0700

    ahihi

--- lib/widgets/popup/product_voucer_popup.dart
+++ lib/widgets/popup/product_voucer_popup.dart
@@ -13,6 +13,7 @@ class TodoPopupCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         color: DesignCourseAppTheme.nearlyWhite,
         child: Container(
+          padding: EdgeInsets.zero,
           constraints:
               BoxConstraints(maxHeight: MediaQuery.of(context).size.height / 2),
           child: Padding(
@@ -20,7 +21,6 @@ class TodoPopupCard extends StatelessWidget {
             child: Column(
               children: [
                 const _HeaderVoucher(),
-                const Divider(),
                 const _VoucherList().expand(),
               ],
             ),
@@ -70,6 +70,55 @@ class _VoucherListState extends State<_VoucherList> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 100, itemBuilder: (context, index) => Container());
+      itemCount: 100,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: InkWell(
+          onTap: () {},
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          hoverColor: context.theme.primaryColor.withOpacity(0.1),
+          child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: Colors.grey.withOpacity(0.2)),
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey.withOpacity(0.2),
+                    ),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.grey.withOpacity(0.2)),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey.withOpacity(0.2),
+                        radius: 50,
+                        backgroundImage: const AssetImage("assets/images/lix4.jpg",),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text("Khuyến mãi $index"),
+                    ),
+                    Text("-"),
+                    Text("-"),
+                    Text("-"),
+                  ],
+                ),
+              )),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+    );
   }
 }
